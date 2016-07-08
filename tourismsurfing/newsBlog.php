9e3729ab9ba6c50fc8a2a13a79efb89859806ec6
epiccookierawr <billchou@alumni.ubc.ca>
2016-07-07 19:28:04 -0700

    updated blog template

--- tourismsurfing/newsBlog.php
+++ tourismsurfing/newsBlog.php
@@ -71,32 +71,77 @@
           printRow('',$picUploaded, $content,$date);
           printRow('','News/2nd.png', $content,$date);
           printRow('','News/3rd.png', $content,$date);
+          */
 
-*/
-          $blogs = json_decode(file_get_contents('blog.json'),true);
-          if (empty($blogs))  $blogs = array();
-          $count = 1;
+          $emptyContent = '
+           <p style="color:rgb(255,247,153); font-size: 150%; padding: 0px 0px 20px 0px;"> empty blog</p>
+           <p> This section is available for a new blog, please submit your blog in the USER BLOG webpage</p>';
 
-          foreach($blogs AS $index => $blog) {
-            $content = '
-             <p style="color:rgb(255,247,153); font-size: 150%; padding: 0px 0px 20px 0px;">'.$blog['name'].', '.$blog['gender'].'</p>
-             <p> Email: '.$blog['email'].' </br>
-                Message: '.$blog['message'].'
-             </p>';
+          $fileCheck = filesize('blog.json');
 
-            $picUploaded = "upload/blogImage/".$blog['blogImg'];
+          if(!$fileCheck){
+            printRow('','images/News/1st.png', $emptyContent,$date);
+            printRow('','images/News/2nd.png', $emptyContent,$date);
+            printRow('','images/News/3rd.png', $emptyContent,$date);
+          }
 
-            $blogDate = date('F', strtotime($blog['date'])) . '<br/>' . date('Y', strtotime($blog['date']));
+          else {
+            $blogs = json_decode(file_get_contents('blog.json'),true);
+            if (empty($blogs))  $blogs = array();
+        //    $count = 1;
+            $numberBlog = count($blogs);
+
+            if($numberBlog > 3){
+              $numberBlog = 3;
+            }
+            echo $numberBlog;
+          //  echo var_dump($blogs);
+          /*  foreach($blogs AS $index => $blog) {
+              $content = '
+              <p style="color:rgb(255,247,153); font-size: 150%; padding: 0px 0px 20px 0px;">'.$blog['name'].', '.$blog['gender'].'</p>
+              <p> Email: '.$blog['email'].' </br>
+                  Message: '.$blog['message'].'
+                  </p>';
+
+                  $picUploaded = "upload/blogImage/".$blog['blogImg'];
+
+                  $blogDate = date('F', strtotime($blog['date'])) . '<br/>' . date('Y', strtotime($blog['date']));
             $date = '
             <div id="circle"><img src="images/News/circle.png"></div>
             <div id="date"><h2>'.$blogDate.'</h2></div>
             ';
             printRow('',$picUploaded, $content,$date);
 
+              if ($count >= ONE_BLOG_DISPLAY) break;
+          } */
 
-            if ($count >= MAX_BLOG_DISPLAY) break;
-            $count++;
-          }
+
+          //  echo var_dump($blog);
+            for ($i = 0; $i < $numberBlog; $i++) {
+
+              $content = '
+              <p style="color:rgb(255,247,153); font-size: 150%; padding: 0px 0px 20px 0px;">'.$blogs[$i]['name'].', '.$blogs[$i]['gender'].'</p>
+              <p> Email: '.$blogs[$i]['email'].' </br>
+                  Message: '.$blogs[$i]['message'].'
+                  </p>';
+
+                  $picUploaded = "upload/blogImage/".$blogs[$i]['blogImg'];
+
+                  $blogDate = date('F', strtotime($blogs[$i]['date'])) . '<br/>' . date('Y', strtotime($blogs[$i]['date']));
+            $date = '
+            <div id="circle"><img src="images/News/circle.png"></div>
+            <div id="date"><h2>'.$blogDate.'</h2></div>
+            ';
+            printRow('',$picUploaded, $content,$date);
+
+            }
+
+            for ($k = $numberBlog; $k < 3; $k++){
+                printRow('','images/News/2nd.png', $emptyContent,$date);
+                echo $k;
+            }
+        }
+
 
 
           printlist($line1,$line2,$line3);
